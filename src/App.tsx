6cddf0a86adc8f61ad715b47ee04d2a99efc1f21
Ivan Obraztsov <ivan-obraztsov@ya.ru>
2024-06-14 14:29:29 +0300

    tablets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.scss';
 import frutolet from './assets/frutolet.png';
-import alien from './assets/alien.svg';
 import sad from './assets/sad.svg';
 import happy from './assets/happy.svg';
 import apple from './assets/apple.png';
@@ -12,13 +11,17 @@ function App() {
   return (
     <Media
       queries={{
-        small: '(max-width: 479px)',
-        medium: '(min-width: 480px)',
+        portarit: '(orientation:portrait)',
+        landscape: '(orientation:landscape)',
+        phone:
+          '(((orientation:portrait) and (max-width:479px)) or ((orientation:landscape) and (max-height:479px)))',
+        tablet:
+          '(((orientation:portrait) and (min-width:480px)) or ((orientation:landscape) and (min-height:480px)))',
       }}
     >
       {(matches) => (
         <Fragment>
-          {matches.small && (
+          {matches.portarit && (
             <div className="sky small">
               <img src={logo} alt="logo" className="logo" />
               <div className="message_wrap">
@@ -36,8 +39,8 @@ function App() {
                 <div className="message">
                   Счастье приходит тогда, когда ты перестаёшь жаловаться на проблемы, которые у тебя
                   есть, и начинаешь благодарить за проблемы, которых у тебя нет
-                  <div className="message_line" />
                 </div>
+                <div className="message_line" />
                 <div className="reactions">
                   <img src={sad} alt="sad" className="reaction" />
                   <div className="reactions_line" />
@@ -45,35 +48,41 @@ function App() {
                 </div>
                 <img src={frutolet} alt="frutolet" className="frutolet" />
               </div>
-              <img src={alien} alt="alien" className="alien" />
             </div>
           )}
-          {matches.medium && (
+          {matches.landscape && (
             <div className="sky">
-              <img src={logo} alt="logo" className="logo" />
+              <img src={logo} alt="logo" className={`logo ${matches.tablet && 'tablet'}`} />
               <div className="message_wrap">
-                <div className="top_block">
+                <div className={`top_block ${matches.tablet && 'tablet'}`}>
                   <div className="apple_wrap">
                     <img src={apple} alt="apple" className="apple" />
                   </div>
                   <div className="title_wrap">
-                    <div className="title">Послание для тебя</div>
+                    <div className={`title ${matches.tablet && 'tablet'}`}>Послание для тебя</div>
                     <div className="title_line" />
                   </div>
                 </div>
-                <div className="message">
+                <div className={`message ${matches.tablet && 'tablet'}`}>
                   Счастье приходит тогда, когда ты перестаёшь жаловаться на проблемы, которые у тебя
                   есть, и начинаешь благодарить за проблемы, которых у тебя нет
-                  <div className="message_line" />
                 </div>
+                <div className="message_line" />
                 <div className="reactions">
-                  <img src={sad} alt="sad" className="reaction" />
+                  <img src={sad} alt="sad" className={`reaction ${matches.tablet && 'tablet'}`} />
                   <div className="reactions_line" />
-                  <img src={happy} alt="happy" className="reaction" />
+                  <img
+                    src={happy}
+                    alt="happy"
+                    className={`reaction ${matches.tablet && 'tablet'}`}
+                  />
                 </div>
-                <img src={frutolet} alt="frutolet" className="frutolet" />
+                <img
+                  src={frutolet}
+                  alt="frutolet"
+                  className={`frutolet ${matches.tablet && 'tablet'}`}
+                />
               </div>
-              <img src={alien} alt="alien" className="alien" />
             </div>
           )}
         </Fragment>
